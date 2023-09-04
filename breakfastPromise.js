677036f4bd345f19027cde876153093614d07b28
devfabien <ish001fabi@gmail.com>
2023-09-04 12:37:19 +0200

    chaining thenables

--- breakfastPromise.js
+++ breakfastPromise.js
@@ -1,8 +1,11 @@
 const breakFast= new Promise(function(resolve,reject){
     let galgic=true;
-    if(galgic==false) resolve(" you can cook noodles");
+    if(galgic==true) resolve(" you can cook noodles");
     reject("you can toast bread")
 });
-breakFast.then((message)=>
-    console.log(`Resolved: ${message}`)
-).then(undefined,(message)=>console.log(`Rejected: ${message}`))
+breakFast.then((message)=>{
+console.log(`Resolved: ${message}`);
+return "wow";
+}
+).catch((message)=>console.log(`Rejected: ${message}`))
+.then((message)=>console.log(message.length))
