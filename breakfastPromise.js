0ecef0d7fe7e8c0333c88ba8f3b93ee6916902d1
devfabien <ish001fabi@gmail.com>
2023-09-05 20:20:00 +0200

    refactoring

--- breakfastPromise.js
+++ breakfastPromise.js
@@ -1,6 +1,6 @@
 const breakFast= new Promise(function(resolve,reject){
     let galgic=true;
-    if(galgic==true) resolve(" you can cook noodles");
+    if(galgic==false) resolve(" you can cook noodles");
     reject("you can toast bread")
 });
 breakFast.then((message)=>{
@@ -8,4 +8,21 @@ console.log(`Resolved: ${message}`);
 return "wow";
 }
 ).catch((message)=>console.log(`Rejected: ${message}`))
-.then((message)=>console.log(message.length))
+
+//error handling with then
+
+breakFast.then((message)=>{
+    console.log(`Resolved: ${message}`);
+    return "wow";
+    }
+    ).then(undefined,(message)=>console.log(`Rejected: ${message}`))
+
+//chaning .thens
+
+
+breakFast.then((message)=>{
+    console.log(`Resolved: ${message}`);
+    return "wow";
+    }
+    ).catch((message)=>console.log(`Rejected: ${message}`))
+    .then((message)=>console.log(message.length))
