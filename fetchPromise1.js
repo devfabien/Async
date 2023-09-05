e511e8217e321a822abf3ee53b47832758ad4f72
devfabien <ish001fabi@gmail.com>
2023-09-05 20:18:56 +0200

    refactor

--- fetchPromise1.js
+++ fetchPromise1.js
@@ -7,7 +7,7 @@ let link= "http://universities.hipolabs.com/search?country=rwanda";
 async function university(){
     await fetch(link).then(async function(res){
         return await res.json();
-    }).then(function(myjson){
+    }).catch(()=>console.log("error in fetching")).then(function(myjson){
         console.log(myjson.map(({name})=>name));
     })
 }
