6d7b47b86daf1cf60ca204a4353524979efef50f
devfabien <ish001fabi@gmail.com>
2023-09-05 12:42:33 +0200

    fetching with async and await

--- fetchPromise1.js
+++ fetchPromise1.js
@@ -1,6 +1,14 @@
-let link= "https://api.publicapis.org/entries";
-fetch(link).then(function(res){
-    return(res.json());
-}).then(function(myjson){
-    console.log(JSON.stringify(myjson))
-})
+let link= "http://universities.hipolabs.com/search?country=rwanda";
+// fetch(link).then(function(res){
+//     return(res.json());
+// }).then(function(myjson){
+//     console.log(myjson.map(({name,country})=>({name,country})))
+// })
+async function university(){
+    await fetch(link).then(async function(res){
+        return await res.json();
+    }).then(function(myjson){
+        console.log(myjson.map(({name})=>name));
+    })
+}
+university();
