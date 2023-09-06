93bf3fecdbc9bfe8e81ac8a151ab5e52ac8a3a6e
devfabien <ish001fabi@gmail.com>
2023-09-06 17:42:29 +0200

    adding a reject method
    adding the function

--- timeout.js
+++ timeout.js
@@ -55,3 +55,17 @@ promise.then(function(data) {
 promise.then(function(data) {
     console.log(data + ' 3');
 });
+
+/* The reject function is used to trigger an error. When you use then, you can give 2 functions. The first 
+function is used when the promise exits successfully. The second function is used when the promise encounters an error. */
+
+
+var promise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        reject('We are all going to die');
+    }, 2000);
+});
+
+promise.then(function success(data) {
+    console.log("resolved",data);
+})
