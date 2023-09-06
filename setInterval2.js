9426f41d82a6dbe31e8714ce02104350fa2e5e81
devfabien <ish001fabi@gmail.com>
2023-09-06 14:37:44 +0200

    adding a counter

--- setInterval2.js
+++ setInterval2.js
@@ -1,10 +1,15 @@
 function job(callback1, callback2) {
-  
+    let count=0;
     setTimeout(() => callback1(),2000);
  
 
 const timer=setInterval(()=> {
     callback2()
+    count++;
+    if(count ==3){
+        clearInterval(timer);  
+      
+    }
 },1000)
     
 }
