7400abab83ff0924f44f564c79fcb25f98c88c71
devfabien <ish001fabi@gmail.com>
2023-09-06 15:13:37 +0200

    Ajax request in jquery

--- syntaxOfPromise.js
+++ syntaxOfPromise.js
@@ -10,5 +10,9 @@ let promise = Q.Promise(function(resolve, reject){
 
 //when using angular
 let promise = $q(function(resolve, reject){
-    
+
 })
+
+//Ajax request in jquery
+
+let promise= $.get('/foo/bar/common')
