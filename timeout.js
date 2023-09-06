e1b3d95c8b4433f9d943f5b1041302775d7a81c8
devfabien <ish001fabi@gmail.com>
2023-09-06 18:00:48 +0200

    binding multiple then methods
    adding the first .then() method and its error handler function

--- timeout.js
+++ timeout.js
@@ -80,3 +80,8 @@ var promise = new Promise(function(resolve, reject) {
         reject('We are all going to die');
     }, 2000);
 });
+promise.then(function success(data) {
+    console.log(data + ' 1');
+}, function error(data) {
+    console.error(data + ' 1');
+});
