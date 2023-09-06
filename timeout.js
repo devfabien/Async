798921562a063d3ba5db9ae81a1835bd80b4090c
devfabien <ish001fabi@gmail.com>
2023-09-06 16:05:48 +0200

    adding a set timeout inside a promise

--- timeout.js
+++ timeout.js
@@ -21,3 +21,15 @@ console.log('Test 2');
 function doSomeStuff() {
     return 1 + 1;
 }
+
+// timeout inside a promise
+
+var promise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve('hello world');
+    }, 2000);
+});
+
+promise.then(function(data) {
+    console.log(data);
+});
