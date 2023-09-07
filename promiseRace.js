998006a20c3d7636687193021792f9303c102119
devfabien <ish001fabi@gmail.com>
2023-09-07 18:52:40 +0200

    Promice.race
    adding the Promise.race description

---
+++ promiseRace.js
@@ -0,0 +1,2 @@
+// Promise.race
+/* Promise.race takes an array of promises. The result is a new promise that resolves or rejects as soon as one of the promises in the given array resolves or rejects. */
