32b0775129342beecc95eaf4b4fc24566b2bfb22
devfabien <ish001fabi@gmail.com>
2023-09-07 15:52:29 +0200

    Chainning promises
    returning the defeat

--- promise1.js
+++ promise1.js
@@ -1,7 +1,72 @@
-  new Promise(function(resolve){
-    console.log('first')
-    resolve();
-    console.log('second');
-  }).then(function(){
-    console.log('third')
-  })
+  // new Promise(function(resolve){
+  //   console.log('first')
+  //   resolve();
+  //   console.log('second');
+  // }).then(function(){
+  //   console.log('third')
+  // })
+
+
+  function job(state) {
+    return new Promise(function(resolve, reject) {
+        if (state) {
+            resolve('success');
+        } else {
+            reject('error');
+        }
+    });
+}
+
+let promise = job(true);
+
+promise
+
+.then(function(data) {
+    console.log(data);
+
+    return job(true);
+})
+
+.then(function(data) {
+    if (data !== 'victory') {
+        return 'Defeat';
+    }
+
+    return job(true);
+})
+
+.then(function(data) {
+    console.log(data);
+})
+
+.catch(function(error) {
+    console.log(error);
+
+    return job(false);
+})
+
+.then(function(data) {
+    console.log(data);
+
+    return job(true);
+})
+
+.catch(function(error) {
+    console.log(error);
+
+    return 'Error caught';
+})
+
+.then(function(data) {
+    console.log(data);
+
+    return new Error('test');
+})
+
+.then(function(data) {
+    console.log('Success:', data.message);
+})
+
+.catch(function(data) {
+    console.log('Error:', data.message);
+});
