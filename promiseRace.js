849c0f6efa871e5007c6e544de3778ef619d9c6e
devfabien <ish001fabi@gmail.com>
2023-09-07 18:54:23 +0200

    Promice.race
    adding the Promise.race exercise

--- promiseRace.js
+++ promiseRace.js
@@ -1,2 +1,12 @@
 // Promise.race
 /* Promise.race takes an array of promises. The result is a new promise that resolves or rejects as soon as one of the promises in the given array resolves or rejects. */
+
+function delay(time) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time, 'success ' + time);
+    });
+}
+
+Promise.race([delay(3000), delay(2000)]).then(function(data) {
+    console.log(data);
+});
