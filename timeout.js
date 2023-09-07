770ed642e1197e987246b183054ec069a5a5fe07
devfabien <ish001fabi@gmail.com>
2023-09-07 11:48:02 +0200

    adding task description

--- timeout.js
+++ timeout.js
@@ -129,3 +129,11 @@ console.log(data)
 })
 
 module.exports = job;
+
+
+/* Let's do a harder exercise. In this code, your function receives a parameter data. You must modify the code below based on the following rules:
+
+Your function must always return a promise
+If data is not a number, return a promise rejected instantly and give the data "error" (in a string)
+If data is an odd number, return a promise resolved 1 second later and give the data "odd" (in a string)
+If data is an even number, return a promise rejected 2 seconds later and give the data "even" (in a string) */
