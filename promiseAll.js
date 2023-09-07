b69031bd0de987de889e2b17151ee70ed81eb440
devfabien <ish001fabi@gmail.com>
2023-09-07 16:19:34 +0200

    the use of Promise.all()
    calling the promise

--- promiseAll.js
+++ promiseAll.js
@@ -8,3 +8,10 @@ function job(delay) {
 }
 
 var promise = Promise.all([job(1000), job(3000), job(500), job(1500)]);
+
+promise.then(function(data) {
+    console.log('All done');
+    data.forEach(function(text) {
+        console.log(text);
+    });
+});
