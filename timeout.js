879af45a3960a818e94a35203f4d40a70467ac41
devfabien <ish001fabi@gmail.com>
2023-09-07 11:49:25 +0200

    checking if an input is not a number and reject

--- timeout.js
+++ timeout.js
@@ -137,3 +137,15 @@ Your function must always return a promise
 If data is not a number, return a promise rejected instantly and give the data "error" (in a string)
 If data is an odd number, return a promise resolved 1 second later and give the data "odd" (in a string)
 If data is an even number, return a promise rejected 2 seconds later and give the data "even" (in a string) */
+function job(data) {
+    return new Promise(function(resolve,reject){
+        if(isNaN(data)){
+        reject("error")
+        }
+        
+        
+        
+    });
+}
+job("test").then(function(vl){console.log(vl)}).catch((error)=>console.log(error));
+job(4).then(function(vl){console.log(vl)}).catch((error)=>console.log(error))
