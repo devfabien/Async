1fc51ed14c378a3943358602d7f7ae93fb885970
devfabien <ish001fabi@gmail.com>
2023-09-17 11:47:03 +0200

    creating a function to get the json format of the data

--- fetchPromise.js
+++ fetchPromise.js
@@ -2,6 +2,10 @@ function get(url){
     return fetch(url,{method:'get'})
 };
 
-
+function getJSON(url){
+    return get(url).then(function(response){
+         return response.json();
+    })
+}
 
 
